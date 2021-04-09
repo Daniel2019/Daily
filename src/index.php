14a70646f8ff11d0093bda025d6ac184042cf8e8
Daniel Soares <Daniel_dandico@outlook.com>
2021-04-08 22:18:59 -0300

    Criação dos arquivos das classes e o CRUD na classe cliente

--- src/index.php
+++ src/index.php
@@ -1,3 +1,14 @@
 <?php
-    
+    require("connection.php");
+    require_once("model/cliente.php");
+
+    $conexao = new ConexaoBanco();
+    $pdo = $conexao->Conectar();
+    $cliente = new Cliente();
+    //$cliente->setNome("nome Daniel");
+    //$cliente->newCliente($pdo);
+    $cliente->setId(3);
+    $cliente->setNome("maria");
+    $cliente->updateCliente($pdo);
+    //$cliente->readClientes($pdo);
 ?>
--- src/model/Cliente.php
+++ src/model/Cliente.php
@@ -1,10 +1,63 @@
 <?php 
-    require("../connection.php");
+    class Cliente{
 
-    echo($var);
+        // Atributos //
+        private $id;
+        private $nome;
 
-    $c = new Conexao();
+        // Métodos Especiais //
+        function getNome(){
+            return $this->nome;
+        }
 
-    $c->conectar();
-    $c-die();
+        function setNome($nome){
+            $this->nome = $nome; 
+        }
+
+        function getId(){
+            return $this->id;
+        }
+
+        function setId($id){
+            $this->id = $id;
+        }
+
+        //Funços //
+        public function newCliente($pdo){
+            $sql = "INSERT INTO cliente(nome) VALUES (:nome)";
+            $insert = $pdo->prepare($sql);
+            $insert->bindValue(':nome', $this->getNome());
+            $insert->execute();
+            $this->readClientes($pdo);
+        }
+
+        public function updateCliente($pdo){
+            $sql = "UPDATE cliente SET nome = :nome WHERE id = :id";
+            $update = $pdo->prepare($sql);
+            $update->bindValue(':nome', $this->getNome());
+            $update->bindValue(':id', $this->getId());
+            $update->execute();
+            $this->readClientes($pdo);
+        }
+
+        public function deleteCliente($pdo){   
+            $sql = "DELETE FROM cliente WHERE id = :id";
+            $delete = $pdo->prepare($sql);
+            $delete->bindValue(':id', $this->getId());
+            $delete->execute();
+            $this->readClientes($pdo);
+        }
+
+        public function readClientes($pdo){
+            $sql = "select * from cliente";
+            $read = $pdo->prepare($sql);
+            $read->execute();
+            $data = $read->fetchAll();
+            for($i = 0; $i < count($data); $i = $i + 1){
+                $valor = $data[$i];
+                echo($valor['nome']);
+            }
+            
+        }
+    }
 ?>
